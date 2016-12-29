e4fa114cbb0c907b97d29674969ec5bfdac0dbd3
chtita <marinahorner91@gmail.com>
2016-12-29 18:27:59 +0100

    added forms, edit and add button

--- blog/views.py
+++ blog/views.py
@@ -3,12 +3,47 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
+from django.shortcuts import redirect
 
-#how do I want the blogposts to be ordered?
+#how do I want the blogposts to be filtered?
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+#what do I want once I click on a post-link - either go to the post or get a 404
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+#give me a little plus sign so that I can add a new post
+# also check if I'm allowed to do that
+    # if yes: check this stuff to validate and once I'm done, take me to the post
+    # if no: go to the edit page
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+#allow me to edit an existing blog post 
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
